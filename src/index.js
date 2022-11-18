56a7e2cba6cfbfa9735f256adafef733a0f4f8d0
janae.wel <janae.wel@gmail.com>
2022-11-17 23:56:21 -0500

    added React!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Hello from "./Hello";
 import './style.css';
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Hello />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
   },
   devServer: {
     static: {
@@ -20,10 +21,23 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
         test: /\.css$/i,
         include: path.resolve(__dirname, 'src'),
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
 }
